3c7c5910d6aaaec2a8e96ecfd19d51547a9d9cd1
F5Prince <f5prince0123@gmail.com>
2024-12-29 17:42:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';  // npm install d3 or yarn add d3
 import f3 from 'family-chart';  // npm install family-chart@0.2.1 or yarn add family-chart@0.2.1
 import 'family-chart/styles/family-chart.css';
 
-fetch('Family DATA.json')
+fetch('https://github.com/F5Prince/family-chart-V1.1/blob/main/Family-DATA.json')
   .then(res => res.json())
   .then(data => create(data))
   .catch(err => console.error(err))
